324234852302cef5735cdcaeb79ff69c434adf11
Zemian Deng <zemiandeng@gmail.com>
2019-03-05 11:26:05 -0500

    Add hello-git files

---
+++ sysinfo.py
@@ -0,0 +1,17 @@
+import platform, os, sys
+
+print(platform.machine())
+print(platform.version())
+print(platform.platform())
+print(platform.uname())
+print(platform.system())
+print(platform.processor())
+print(platform.python_version())
+print('')
+
+print(os.uname())
+print('')
+
+print(sys.version)
+print(sys.version_info)
+print('')
